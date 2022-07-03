315b80fc5d0c198fdcf1824ae9a8e358d526d15e
cheatfate <eugene.kabanov@status.im>
2022-07-03 22:24:29 +0300

    Improve chronos metrics HTTP server.

--- tests/chronos_server_tests.nim
+++ tests/chronos_server_tests.nim
@@ -4,11 +4,155 @@
 #   * Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
 # at your option. This file may not be copied, modified, or distributed except according to those terms.
 
-import ../metrics, ../metrics/chronos_httpserver,
-  os, osproc
+import std/[uri, unittest]
+import chronos, chronos/apps/http/httpclient
+import ../metrics, ../metrics/chronos_httpserver
 
-startMetricsHttpServer()
-sleep(1000)
-when defined(metrics):
-  doAssert execCmd("curl http://127.0.0.1:8000/metrics") == 0
+suite "Chronos metrics HTTP server test suite":
 
+  proc httpClient(url: string): Future[HttpResponseTuple] {.async.} =
+    let session = HttpSessionRef.new()
+    try:
+      let resp = await session.fetch(parseUri(url))
+      return resp
+    finally:
+      await session.closeWait()
+
+  test "new()/close() test":
+    proc test() {.async.} =
+      let server = MetricsHttpServerRef.new("127.0.0.1", Port(8080)).get()
+      block:
+        let status = await server.status()
+        check status == MetricsHttpServerStatus.Stopped
+      await server.close()
+      block:
+        let status = await server.status()
+        check status == MetricsHttpServerStatus.Closed
+    waitFor test()
+  test "new()/start()/stop()/close() test":
+    proc test() {.async.} =
+      let server = MetricsHttpServerRef.new("127.0.0.1", Port(8080)).get()
+      block:
+        let status = await server.status()
+        check status == MetricsHttpServerStatus.Stopped
+      await server.start()
+      block:
+        let status = await server.status()
+        check status == MetricsHttpServerStatus.Running
+      await server.stop()
+      block:
+        let status = await server.status()
+        check status == MetricsHttpServerStatus.Stopped
+      await server.close()
+      block:
+        let status = await server.status()
+        check status == MetricsHttpServerStatus.Closed
+    waitFor test()
+  test "new()/start()/response/stop()/start()/response/stop()/close() test":
+    proc test() {.async.} =
+      let server = MetricsHttpServerRef.new("127.0.0.1", Port(8080)).get()
+      block:
+        let status = await server.status()
+        check status == MetricsHttpServerStatus.Stopped
+      await server.start()
+      block:
+        let status = await server.status()
+        check status == MetricsHttpServerStatus.Running
+      block:
+        let resp = await httpclient("http://127.0.0.1:8080/health")
+        check:
+          resp.status == 200
+          resp.data.bytesToString == "OK"
+      await server.stop()
+      block:
+        let status = await server.status()
+        check status == MetricsHttpServerStatus.Stopped
+      await server.start()
+      block:
+        let status = await server.status()
+        check status == MetricsHttpServerStatus.Running
+      block:
+        let resp = await httpclient("http://127.0.0.1:8080/health")
+        check:
+          resp.status == 200
+          resp.data.bytesToString == "OK"
+      await server.stop()
+      block:
+        let status = await server.status()
+        check status == MetricsHttpServerStatus.Stopped
+      await server.close()
+      block:
+        let status = await server.status()
+        check status == MetricsHttpServerStatus.Closed
+    waitFor test()
+  test "new()/start()/close() test":
+    proc test() {.async.} =
+      let server = MetricsHttpServerRef.new("127.0.0.1", Port(8080)).get()
+      block:
+        let status = await server.status()
+        check status == MetricsHttpServerStatus.Stopped
+      await server.start()
+      block:
+        let status = await server.status()
+        check status == MetricsHttpServerStatus.Running
+      await server.close()
+      block:
+        let status = await server.status()
+        check status == MetricsHttpServerStatus.Closed
+    waitFor test()
+  test "HTTP 200/responses check test":
+    proc test() {.async.} =
+      let server = MetricsHttpServerRef.new("127.0.0.1", Port(8080)).get()
+      block:
+        let status = await server.status()
+        check status == MetricsHttpServerStatus.Stopped
+      await server.start()
+      block:
+        let status = await server.status()
+        check status == MetricsHttpServerStatus.Running
+      block:
+        let resp = await httpclient("http://127.0.0.1:8080/metrics")
+        check:
+          resp.status == 200
+          len(resp.data) > 0
+
+      block:
+        let resp = await httpclient("http://127.0.0.1:8080/health")
+        check:
+          resp.status == 200
+          resp.data.bytesToString() == "OK"
+      await server.stop()
+      block:
+        let status = await server.status()
+        check status == MetricsHttpServerStatus.Stopped
+      await server.close()
+      block:
+        let status = await server.status()
+        check status == MetricsHttpServerStatus.Closed
+    waitFor test()
+  test "HTTP 404/response check test":
+    proc test() {.async.} =
+      let server = MetricsHttpServerRef.new("127.0.0.1", Port(8080)).get()
+      block:
+        let status = await server.status()
+        check status == MetricsHttpServerStatus.Stopped
+      await server.start()
+      block:
+        let status = await server.status()
+        check status == MetricsHttpServerStatus.Running
+
+      block:
+        let resp = await httpclient("http://127.0.0.1:8080/somePath")
+        check:
+          resp.status == 404
+          len(resp.data) > 0
+
+      await server.stop()
+      block:
+        let status = await server.status()
+        check status == MetricsHttpServerStatus.Stopped
+      await server.close()
+      block:
+        let status = await server.status()
+        check status == MetricsHttpServerStatus.Closed
+    waitFor test()
