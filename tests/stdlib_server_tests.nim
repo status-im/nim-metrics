a4ee8985742933be40dd8cd389fbfb0e4153acbd
Dustin Brody <tersec@users.noreply.github.com>
2022-07-18 19:29:04 +0000

    unused import message evidently incorrect

--- tests/stdlib_server_tests.nim
+++ tests/stdlib_server_tests.nim
@@ -1,11 +1,11 @@
-# Copyright (c) 2021-2022 Status Research & Development GmbH
+# Copyright (c) 2021 Status Research & Development GmbH
 # Licensed and distributed under either of
 #   * MIT license: http://opensource.org/licenses/MIT
 #   * Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
 # at your option. This file may not be copied, modified, or distributed except according to those terms.
 
 import ../metrics, ../metrics/stdlib_httpserver,
-  os
+  os, httpclient
 
 startMetricsHttpServer()
 sleep(1000)
