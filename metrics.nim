cc4f1bbdc717304bd3652b751e2ea3c1da0054cd
tersec <tersec@users.noreply.github.com>
2023-06-14 23:23:26 +0000

    remove Nim 1.2 and 1.4 support (#67)
    * remove Nim 1.2 and 1.4 support
    
    * stew/shims/stddefects is unnecessary
    
    * update lockfile
    
    * rm stew from lockfile
    
    * re-add stew to lockfile with updated commit reference
    
    * rm lockfile
    
    * rm past lockfile

--- metrics.nim
+++ metrics.nim
@@ -1,4 +1,4 @@
-# Copyright (c) 2019-2022 Status Research & Development GmbH
+# Copyright (c) 2019-2023 Status Research & Development GmbH
 # Licensed and distributed under either of
 #   * MIT license: http://opensource.org/licenses/MIT
 #   * Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
@@ -22,12 +22,6 @@ when defined(metrics):
   import algorithm, hashes, random, sequtils, strutils,
     metrics/common
 
-  # dealings with deprecated defects
-  # Nim still claims stddefects is an unused import, sigh
-  # although we really use it, in templates sandwich   
-  {.warning[UnusedImport]:off.}
-  import stew/shims/stddefects
-  
   export tables # for custom collectors that need to work with the "Metrics" type
   when defined(posix):
     import posix
